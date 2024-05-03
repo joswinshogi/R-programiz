e0c0787b1528028fdd7b00cf68d1ba46eacf37eb
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2024-05-03 08:07:47 +0530

    Update Logistic-Regression.R

--- R-Programiz/Logistic-Regression.R
+++ R-Programiz/Logistic-Regression.R
@@ -6,8 +6,8 @@ split = createDataPartition(biopsy$class, p=0.7,list = FALSE, times = 1)
 train = biopsy[split,]
 test = biopsy[-split,]
 model = glm(class ~ V1 + V2, data = train, family = 'binomial')
-test$predicted = predict(model, newdata = test, type='response')
-predicted_labels = ifelse(test$predicted > 0.5, 'malignant','benign')
+test_predicted = predict(model, newdata = test, type='response')
+predicted_labels = ifelse(test_predicted > 0.5, 'malignant','benign')
 predicted_labels = factor(predicted_labels, levels = c('benign','malignant'))
 cm = confusionMatrix(test$class, predicted_labels)$table
 print('Confusion Matrix: ')
